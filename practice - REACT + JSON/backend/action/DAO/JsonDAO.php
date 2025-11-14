d1bb0d85d06530efd485b9733917945c23073f45
Baltazius <joel_maillyniquay@hotmail.com>
2025-11-14 08:04:21 -0500

    update practice

--- practice - REACT + JSON/backend/action/DAO/JsonDAO.php
+++ practice - REACT + JSON/backend/action/DAO/JsonDAO.php
@@ -2,6 +2,13 @@
     class JsonDAO {
         public static function getJson($index){
             $url = "../shopcvm-frontend/src/assets/json/game-" . $index . "json";
+            if ($index == 2) {
+                $value = rand(0, 100);
+                // Call #666
+            }
+            else {
+
+            }
 
         } 
     }
